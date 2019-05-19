b540ff21344c2f105b0ee7cdf6d57abedf6d803d
myriam2008-- <mahfoudmya@gmail.com-->
2019-05-19 11:09:32 +0300

    commiting script4

---
+++ Exercises/Javascript-Basics-01/script4.js
@@ -0,0 +1,6 @@
+var name = prompt('What is your name?');
+var surName = prompt('What is your surName?');
+var city = prompt('Where are you from?')
+
+
+alert('Hello, ' + name + ' ' + surName + ' ' + 'from ' + city + ' nice to meet you!'); 

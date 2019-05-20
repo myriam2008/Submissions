8ac5a1e788bd29398aad892afc0fbc3a811b65be
myriam2008-- <mahfoudmya@gmail.com-->
2019-05-20 14:54:03 +0300

    commit scrip7

---
+++ Exercises/Javascript-Basics-01/script7.js
@@ -0,0 +1,8 @@
+var a = prompt("Please enter your shoe size");
+var b = prompt("Please enter your birth year");
+alert ( myFunction ( ) );
+function myFunction() {
+  return ( (a * 2 ) + (5 * 50) - b + 1766  )
+     
+ }
+ 

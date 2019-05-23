c845e86f293fa746d9431a4381b585243416a90e
myriam2008-- <mahfoudmya@gmail.com-->
2019-05-23 16:45:24 +0300

    commiting js basics 4

--- Exercises/Javascript-Basics-04/questions.js
+++ Exercises/Javascript-Basics-04/questions.js
@@ -1,101 +1,114 @@
 
-const stringSize = (text) => {
+const stringSize = (text) => { return text.length;
 
 }
-const replaceCharacterE = (text) => {
+const replaceCharacterE = (text) => { return text.replace("e", " ");
 
 }
-const concatString = (text1, text2) => {
+const concatString = (text1, text2) => { return text1.concat(text2);
 
 }
-const showChar5 = (text) => {
+const showChar5 = (text) => { return text[4];
 
 }
-const showChar9 = (text) => {
+const showChar9 = (text) => { return text.substring(0,9);
 
 }
-const toCapitals = (text) => {
+const toCapitals = (text) => { return text.toUpperCase();
 
 }
-const toLowerCase = (text) => {
+const toLowerCase = (text) => { return text.toLowerCase();
 
 }
-const removeSpaces = (text) => {
+const removeSpaces = (text) => { return text.trim();
 
 }
-const IsString = (text) => {
-
-}
-
-const getExtension = (text) => {
-
-}
-const countSpaces = (text) => {
-
-}
-const InverseString = (text) => {
+const IsString = (text) => { return (text=='string');
 
 }
 
-const power = (x, y) => {
+const getExtension = (text) => { return text.split('.').pop();
 
 }
-const absoluteValue = (num) => {
+const countSpaces = (text) => { return (text.split(" ").length - 1);
 
 }
-const absoluteValueArray = (array) => {
+const InverseString = (text) => { return str.split("").reverse().join("");
 
 }
-const circleSurface = (radius) => {
+
+const power = (x, y) => { return Math.pow(x, y);
 
 }
-const hypothenuse = (ab, ac) => {
+const absoluteValue = (num) => { return Math.absolute (num);
+
+}
+const absoluteValueArray = (array) => { return Math.absolute([array])
+
+}
+const circleSurface = (radius) => { return radius * radius * Math.PI;
+
+}
+const hypothenuse = (ab, ac) => { return Math.hypot(ab,ac);
 
 }
 const BMI = (weight, height) => {
 
 }
 
-const createLanguagesArray = () => {
+const createLanguagesArray = () => { const languages = ["Html","CSS","Java","PHP"];
+return languages;
 
 }
 
-const createNumbersArray = () => {
+const createNumbersArray = () => { const numbers = [0,1,2,3,4,5];
+return numbers;
 
 }
 
 const replaceElement = (languages) => {
-
+    languages[2]='Javascript';
+    return languages;
 }
 
 const addElement = (languages) => {
-
+    languages.push("Ruby", "Python");
+    return languages; 
 }
 
 const addNumberElement = (numbers) => {
-
+    numbers.unshift("-2", "-1"); 
+    return numbers;
 }
 
 const removeFirst = (languages) => {
+    languages.shift();
+    return languages;
 
 }
 
 const removeLast = (languages) => {
+    languages.slice(-1);
+    return languages;
 
 }
 
-const convertStrToArr = (social_arr) => {
+const convertStrToArr = (social_arr) => { social_arr.split(',');
+return social_arr;
 
 }
 
-const convertArrToStr = (languages) => {
+const convertArrToStr = (languages) => { languages.toString();
+    return languages;
 
 }
 
-const sortArr = (social_arr) => {
+const sortArr = (social_arr) => { social_arr.sort();
+    return social_arr;
 
 }
 
-const invertArr = (social_arr) => {
+const invertArr = (social_arr) => { social_arr.reverse();
+    return social_arr;
 
 }

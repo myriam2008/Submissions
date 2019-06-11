9b60e40f2609a74e2858a188704bfd02884664f4
myriam2008-- <mahfoudmya@gmail.com-->
2019-06-11 19:57:47 +0300

    commit step 4

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -23,4 +23,32 @@ app.get('/test', (req, res) => {
   });
 
 
+  app.get('/hello/:id?',  (req, res) =>{
+      if(req.params.id !== undefined){
+    res.send ({
+        status:200, 
+        message:"Hello," + req.params.id
+    })}
+    else {
+        res.send ({
+            status:200, 
+            message:"Hello," 
+        })}
+    
+  })
+
+
+  app.get('/search',  (req, res) =>{
+    if(req.query.s !== undefined && req.query.s !== ""){
+        res.send({status:200, message:"ok", data:req.query.s})
+         
+        
+        } 
+        else {
+            res.send({status:500, error:true, message:"you have to provide a search"})
+        }
+    }
+)
+  
+
   app.listen(3000)

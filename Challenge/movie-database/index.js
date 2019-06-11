66fd7f08412778ae0b3df4a271def17aa973f816
myriam2008-- <mahfoudmya@gmail.com-->
2019-06-11 17:41:09 +0300

    commit step 3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -4,4 +4,23 @@ const app = express()
 
 app.get('/', (req, res) => res.send('OK'))
 
-app.listen(3000)
+
+
+app.get('/test', (req, res) => {
+    res.status(200).send({
+        status:200,
+        message:"ok"
+    })
+  });
+  var today = new Date();
+  var Time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  app.get('/time', (req, res) => {
+      
+    res.status(200).send({
+        status:200,
+        message:Time
+    })
+  });
+
+
+  app.listen(3000)

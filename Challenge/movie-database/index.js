57ed1af3d089a1bee14a39bea756367be67e7e11
myriam2008-- <mahfoudmya@gmail.com-->
2019-06-12 11:58:05 +0300

    step 5

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+
 
 app.get('/', (req, res) => res.send('OK'))
 
@@ -50,5 +57,21 @@ app.get('/test', (req, res) => {
     }
 )
   
+
+
+app.get('/movies/create', (req, res) =>{
+
+})
+
+app.get('/movies/read', (req, res) => {
+    res.status(200).send({
+        status:200,
+        data:movies
+    })
+  });
+  
+  app.get('/movies/update', (req, res)=>{})
+  app.get('/movies/delete', (req, res)=>{})
+
 
   app.listen(3000)

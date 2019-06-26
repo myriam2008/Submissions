ce2e343fa6d60c0a505051615544a32234b430eb
myriam2008-- <mahfoudmya@gmail.com-->
2019-06-26 11:32:01 +0300

    commit tasks.js till step 5

--- Challenge/NodeJS/tasks.js
+++ Challenge/NodeJS/tasks.js
@@ -31,15 +31,38 @@ function startApp(name){
  * This function  then directs to other functions
  * 
  * @param  {string} text 
+  process.stdin.setEncoding('utf8');
+  process.stdin.on('data', onDataReceived);
+  console.log(`Welcome to ${name}'s application!`)
+  console.log("--------------------")
+}
+
+
+/**
+ * Decides what to do depending on the data that was received
+ * This function receives the input sent by the user.
+ * 
+ * For example, if the user entered 
+ * ```
+ * node tasks.js batata
  * @returns {void}
  */
+var tasksArr = [
+  {name:'task1.',
+done:'no'}, 
+{name:'task2.',
+done:'yes'}, 
+{name:'task3.', done: 'no'},
+{name:'task4.', done: 'no'}];
+
 
 function onDataReceived(text) {
-
+  textsplit=text.trim().split(' ');
   var arraySTR = text.trim().split(" ")
   console.log(arraySTR)
   var string  = text.trim('').split(" ") 
-  
+  var newVar = text.trim('').split(" ")
+
   if (text === 'quit\n' || text === 'exit\n') {
     quit();
   }
@@ -49,17 +72,42 @@ function onDataReceived(text) {
     else if(text === 'help\n'){
       help();
     }
+    else if(text === 'list\n'){
+      list();
+    }
+
+    else if (text === 'remove\n') { 
+      remove();
+  }
+  else if(text === 'remove1\n') {
+    removeFirst();
+  }
+
+  else if(text==='edit\n' || textsplit[0] === 'edit'){
+    edit(text,textsplit);
+}
+
+  else if(text === 'remove2\n') {
+    removeSecond();
+  }
+
+  else if(textsplit[0]==='add') {
+    add(text);
+  }
+  
+
   else{
     unknownCommand(text);
   }
 }
 
+
+
 /**
  * prints "unknown command"
  * This function is supposed to run when all other commands have failed
  *
- * @param  {string} c the text received
- * @returns {void}
+ * 
  * 
  */
 function unknownCommand(c){
@@ -69,8 +117,8 @@ function unknownCommand(c){
 
 /**
  * 
- * @param {string} t 
- * @retu
+ *  @param  {string} c the text received
+ * @returns {void}
  */
 
 function hello(t){
@@ -81,12 +129,59 @@ function hello(t){
 
 function help(){
   console.log('choose one of the following commands:\nhelp\nhello\nexit')
-}
+}   
 
 function quit(){
   console.log('Quitting now, goodbye!')
   process.exit();
 }
+function list (text) {
+  for (i=0; i<tasksArr.length; i++) {
+console.log((i+1)+')'+tasksArr[i])
+  }
+};
+
+function remove() {
+  tasksArr.splice(-1,1);
+}
+
+function removeFirst() {
+  tasksArr.pop();
+}
+
+function removeSecond() {
+  tasksArr.shift();
+}
+
+function edit(text,textsplit){
+  if(text==='edit\n'){
+      console.log("\n"+"Error")}
+  
+
+else if(textsplit[0]==='edit' && isNaN(textsplit[1])){
+  //console.log(task)
+  console.log(textsplit)
+  tasksArr[tasksArr.length-1].name = textsplit[1]
+tasksArr[tasksArr.length-1].done=textsplit[2] 
+
+}
+
+else if(textsplit[0]==='edit' && textsplit[1]==='1'){
+  //console.log(textsplit[2]+textsplit[3] + textsplit.length);
+  tasksarr[0].name=textsplit[2];
+  tasksarr[0].done=textsplit[3];
+  list(text)
+}
+
+function add(text) {
+if (text === 'add\n') {
+  console.log('error')
+
+
+  return "error"
+} }}
+
+
 
 // The following line starts the application
 startApp("myriam")
